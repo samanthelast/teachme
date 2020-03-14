0a39473ebd988f3c4a575800bceaf8c203b96f2b
Saman Ariyanpour <samanthelast@gmail.com>
2020-03-14 23:50:31 +0330

    modern profile

--- lib/screens/Profile_screen.dart
+++ lib/screens/Profile_screen.dart
@@ -6,6 +6,7 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  String view = "grid";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,10 +19,121 @@ class _ProfileScreenState extends State<ProfileScreen> {
               child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Row(
+          Stack(
+            alignment: Alignment.topCenter,
             children: <Widget>[
               Container(
-                margin: EdgeInsets.only(top: 15, left: 30, right: 30),
+                width: MediaQuery.of(context).size.width,
+                height: 160,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: new AssetImage('assets/images/paniz_back2.jpg'),
+                  ),
+                ),
+              ),
+              Container(
+                height: 320,
+                width: MediaQuery.of(context).size.width - 60,
+                margin: EdgeInsets.only(top: 100),
+                child: Card(
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 80),
+                      Container(
+                        
+                        
+                        child: Text(
+                          'Paniz Ariyanmanesh',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Container(
+                       
+                        margin: EdgeInsets.only(top: 30),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Column(
+                              children: <Widget>[
+                                Text(
+                                  '302',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  'POSTS',
+                                  style: TextStyle(fontSize: 10),
+                                )
+                              ],
+                            ),
+                            Column(
+                              children: <Widget>[
+                                Text(
+                                  '10K',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  'FOLLOWERS',
+                                  style: TextStyle(fontSize: 10),
+                                )
+                              ],
+                            ),
+                            Column(
+                              children: <Widget>[
+                                Text(
+                                  '120',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  'FOLLOWING',
+                                  style: TextStyle(fontSize: 10),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 30),
+                      Container(
+                        alignment: Alignment.topLeft,
+                        margin: EdgeInsets.only(left: 30),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              '📈Trader',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              '🧠Mentor',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              '👥Let\'s grow together',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 65),
                 height: 100.0,
                 width: 100.0,
                 decoration: BoxDecoration(
@@ -33,104 +145,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
                     fit: BoxFit.cover,
                   ),
                 ),
-              ),
-              Expanded(
-                flex: 1,
-                child: Container(
-                  margin: EdgeInsets.only(top: 30, right: 30),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Column(
-                        children: <Widget>[
-                          Text(
-                            '302',
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            'POSTS',
-                            style: TextStyle(fontSize: 10),
-                          )
-                        ],
-                      ),
-                      Column(
-                        children: <Widget>[
-                          Text(
-                            '10.3K',
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            'FOLLOWERS',
-                            style: TextStyle(fontSize: 10),
-                          )
-                        ],
-                      ),
-                      Column(
-                        children: <Widget>[
-                          Text(
-                            '120',
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            'FOLLOWING',
-                            style: TextStyle(fontSize: 10),
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+              )
             ],
-          ),
-          SizedBox(height: 15),
-          Container(
-            margin: EdgeInsets.only(left: 30),
-            child: Text(
-              'Paniz Ariyanmanesh',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-          ),
-          SizedBox(height: 10),
-          Container(
-            
-            margin: EdgeInsets.only(left: 30),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  '📈Trader',
-                  style: TextStyle(fontSize: 18),
-                ),
-                SizedBox(height: 5,),
-                Text(
-                  '🧠Mentor',
-                  style: TextStyle(fontSize: 18),
-                ),
-                SizedBox(height: 5,),
-                Text(
-                  '👥Let\'s grow together',
-                  style: TextStyle(fontSize: 18),
-                ),
-                
-              ],
-              
-            ),
-          ),
-          SizedBox(height: 15),
-          Container(
-            margin: EdgeInsets.only(left: 30, right: 30),
-            width: MediaQuery.of(context).size.width,
-            child: RaisedButton(
-              onPressed: () {},
-              child: Text(
-                'Edit Profile',
-              ),
-              color: Colors.white,
-            ),
           ),
           SizedBox(height: 15),
           Container(
@@ -138,36 +154,51 @@ class _ProfileScreenState extends State<ProfileScreen> {
             child: SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
                 child: Row(children: <Widget>[
-                  
-                  Story('assets/images/story-tutorial.jpg', 'Tutorials',Colors.blue),
+                  Story('assets/images/story-tutorial.jpg', 'Tutorials',
+                      Colors.blue),
                   SizedBox(
                     width: 10,
                   ),
-                  Story('assets/images/story-travel.jpg', 'Travel',Colors.pink),
+                  Story(
+                      'assets/images/story-travel.jpg', 'Travel', Colors.pink),
                   SizedBox(
                     width: 10,
                   ),
-                  Story('assets/images/story-mindset.jpg', 'Mindset',Colors.blue),
+                  Story('assets/images/story-mindset.jpg', 'Mindset',
+                      Colors.blue),
                   SizedBox(
                     width: 10,
                   ),
-                  Story('assets/images/story-book.jpg', 'MyBooks',Colors.blue),
+                  Story('assets/images/story-book.jpg', 'MyBooks', Colors.blue),
                   SizedBox(
                     width: 10,
                   ),
-                  Story('assets/images/story-car.jpg', 'Cars',Colors.pink),
+                  Story('assets/images/story-car.jpg', 'Cars', Colors.pink),
                   SizedBox(
                     width: 10,
                   ),
-                  Story('assets/images/story-vacation.jpg', 'Vacation',Colors.pink),
+                  Story('assets/images/story-vacation.jpg', 'Vacation',
+                      Colors.pink),
                 ])),
-          )
+          ),
+          Divider(),
+          buildImageViewButtonBar(),
+          Divider(height: 0.0),
+          buildImageViewButtonBar(),
+          buildImageViewButtonBar(),
+          buildImageViewButtonBar(),
+          buildImageViewButtonBar(),
+          buildImageViewButtonBar(),
+          buildImageViewButtonBar(),
+          buildImageViewButtonBar(),
+          buildImageViewButtonBar(),
+          buildImageViewButtonBar(),
         ],
       ))),
     );
   }
 
-  Widget Story(String imageAdd, String title,Color borderColor) {
+  Widget Story(String imageAdd, String title, Color borderColor) {
     return Column(
       children: <Widget>[
         Container(
@@ -175,24 +206,54 @@ class _ProfileScreenState extends State<ProfileScreen> {
           width: 70.0,
           margin: EdgeInsets.only(bottom: 10),
           decoration: BoxDecoration(
-            border: Border.all(
-              color: borderColor,
-              width: 3
-            ),
+            border: Border.all(color: borderColor, width: 3),
             shape: BoxShape.circle,
-            color:borderColor,
+            color: borderColor,
             image: new DecorationImage(
               image: new ExactAssetImage(imageAdd),
               fit: BoxFit.cover,
-
             ),
           ),
         ),
         Text(
           title,
           style: TextStyle(fontSize: 15),
-        )
+        ),
       ],
     );
+  }
+
+  Row buildImageViewButtonBar() {
+    Color isActiveButtonColor(String viewName) {
+      if (view == viewName) {
+        return Colors.blueAccent;
+      } else {
+        return Colors.black26;
+      }
+    }
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        IconButton(
+          icon: Icon(Icons.school, color: isActiveButtonColor("grid")),
+          onPressed: () {
+            changeView("grid");
+          },
+        ),
+        IconButton(
+          icon: Icon(Icons.camera, color: isActiveButtonColor("feed")),
+          onPressed: () {
+            changeView("feed");
+          },
+        ),
+      ],
+    );
+  }
+
+  changeView(String viewName) {
+    setState(() {
+      view = viewName;
+    });
   }
 }
